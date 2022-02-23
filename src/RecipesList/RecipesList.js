21442fafd205414e9e57dc1f9c600fe9aabd0861
Jackie <taekwondo.chan@gmail.com>
2022-02-22 17:06:34 -0800

    generate cards from json

--- src/RecipesList/RecipesList.js
+++ src/RecipesList/RecipesList.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './RecipesList.css';
 import RecipeCard from '../RecipeCard/RecipeCard';
+import data from '../data.json';
 
 function RecipesList() {
-	return (
-		<div className="RecipesList">
-			<RecipeCard name="Test1" servings="Servings1" />
-			<RecipeCard name="Test2" servings="Servings2" />
-			<RecipeCard name="Test3" servings="Servings3" />
-			<RecipeCard name="Test4" servings="Servings4" />
-		</div>
-	);
+	const recipes = data.map(({ name, servings }) => {
+		return <RecipeCard key={name} name={name} servings={servings} />;
+	});
+
+	return <div className="RecipesList">{recipes}</div>;
 }
 
 export default RecipesList;
