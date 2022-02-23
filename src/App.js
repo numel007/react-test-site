4eb991a38eb0130b5bf730e2255753fa3ba509c7
Jackie <taekwondo.chan@gmail.com>
2022-02-22 16:59:44 -0800

    add footer comp

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import './Header/Header';
 import Header from './Header/Header';
 import RecipesList from './RecipesList/RecipesList';
+import Footer from './Footer/Footer';
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
 			<RecipesList />
+			<Footer />
 		</div>
 	);
 }
