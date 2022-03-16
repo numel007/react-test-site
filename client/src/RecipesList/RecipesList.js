0737917bd1157145b43081d0e12dada63b29a24d
Jackie <taekwondo.chan@gmail.com>
2022-03-16 16:33:48 -0700

    query getAllRecipes

--- client/src/RecipesList/RecipesList.js
+++ client/src/RecipesList/RecipesList.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
 import './RecipesList.css';
 import RecipeCard from '../RecipeCard/RecipeCard';
 import { useQuery, gql } from '@apollo/client';
@@ -8,12 +7,12 @@ const ALL_RECIPES_QUERY = gql`
 	{
 		getAllRecipes {
 			name
+			servings
 		}
 	}
 `;
 
 function RecipesList() {
-	// REBUILD
 	const { data, loading, error } = useQuery(ALL_RECIPES_QUERY);
 
 	if (loading) return 'Loading...';
@@ -22,11 +21,10 @@ function RecipesList() {
 		return <pre>{error.message}</pre>;
 	}
 
-	console.log(data);
-
-	let recipesArr = data.map(({ id, name, servings }, i) => (
+	let recipesArr = data.getAllRecipes.map(({ id, name, servings }, i) => (
 		<RecipeCard key={i} id={id} name={name} servings={servings} />
 	));
+
 	return <div className="RecipesList">{recipesArr}</div>;
 }
 
