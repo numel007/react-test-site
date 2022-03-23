911a1b5d7ed1884d62ff4a5eecb89922667e9a74
Jackie <taekwondo.chan@gmail.com>
2022-03-23 03:11:14 -0700

    deployment fixes, add nodemon

--- graphql-server/server.js
+++ graphql-server/server.js
@@ -7,7 +7,9 @@ require('dotenv').config();
 
 const cors = require('cors');
 const corsOptions = {
-	origin: 'http://localhost:3000',
+	origin: '*',
+	credentials: true, //access-control-allow-credentials:true
+	optionSuccessStatus: 200,
 };
 
 const startServer = async () => {
